88e17ce082571788d73282d696db7acb99c05a81
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2026-01-03 11:26:42 +0530

    name chnage and add logo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import MT5 from './Pages/MT5'
 import TradingView from './Pages/TradingView'
 import Platforms from './Pages/Platforms'
 import AboutUs from './Pages/AboutUs'
-import ContactUs from './Pages/ContactUs'
+// import ContactUs from './Pages/ContactUs'
 import Indices from './Pages/Indices'
 import Forex from './Pages/Forex'
 import Commodities from './Pages/Commodities'
@@ -48,7 +48,7 @@ function App() {
 
         {/* ================= ABOUT US ================= */}
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        {/* <Route path="/contact-us" element={<ContactUs />} /> */}
 
         {/* ================= INDICES ================= */}
         <Route path="/indices" element={<Indices />} />
